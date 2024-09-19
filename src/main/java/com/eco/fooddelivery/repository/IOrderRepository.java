c4a143ca1e13bfda8605279718d7f3d0ed30d44d
Nicolas <nicolas.echenique.117@gmail.com>
2024-09-19 09:31:21 -0300

    Repository agregando metodo

--- src/main/java/com/eco/fooddelivery/repository/IOrderRepository.java
+++ src/main/java/com/eco/fooddelivery/repository/IOrderRepository.java
@@ -1,5 +1,7 @@
 package com.eco.fooddelivery.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,10 @@ import com.eco.fooddelivery.model.Order;
 @Repository
 public interface IOrderRepository extends JpaRepository< Order, Long >
 {
-    
+    /* Explicacion: JPA relaciona con Java hacia la DB.
+     *
+     * Con "user_userId", indicamos que busque en el atributo "user" del objeto Order (que es la
+     * referencia al User), por el atributo "userId".
+     */
+    List<Order> findByuser_userId( Long userId );
 }
