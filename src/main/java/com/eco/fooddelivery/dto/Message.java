08aae896d80b89de1c2356bf656bb824cc364c5f
Nicolas <nicolas.echenique.117@gmail.com>
2024-09-19 06:33:12 -0300

    Agregando class message

---
+++ src/main/java/com/eco/fooddelivery/dto/Message.java
@@ -0,0 +1,15 @@
+package com.eco.fooddelivery.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Message
+{
+    private String message;
+}
