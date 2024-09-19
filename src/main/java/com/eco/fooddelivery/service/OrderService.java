b1dcf6a673278262af2195458c5bca49144fd093
Nicolas <nicolas.echenique.117@gmail.com>
2024-09-19 06:45:32 -0300

    Services listos

--- src/main/java/com/eco/fooddelivery/service/OrderService.java
+++ src/main/java/com/eco/fooddelivery/service/OrderService.java
@@ -1,38 +1,120 @@
 package com.eco.fooddelivery.service;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.eco.fooddelivery.dto.OrderDto;
+import com.eco.fooddelivery.jwt.model.User;
+import com.eco.fooddelivery.jwt.service.UserService;
 import com.eco.fooddelivery.model.Order;
+import com.eco.fooddelivery.model.Product;
 import com.eco.fooddelivery.repository.IOrderRepository;
 
 @Service
 public class OrderService
 {
     private IOrderRepository iOrderRepository;
+    private UserService userService;
+    private ProductService productService;
 
-    public OrderService( IOrderRepository iOrderRepository )
+
+    public OrderService
+    (
+        IOrderRepository iOrderRepository,
+        UserService userService,
+        ProductService productService
+    )
     {
         this.iOrderRepository = iOrderRepository;
+        this.userService = userService;
+        this.productService = productService;
+    }
+
+    public List<OrderDto> findAll()
+    {
+        List<Order> orders = iOrderRepository.findAll();
+
+        List<OrderDto> orderDto = new ArrayList<>();
+
+        OrderDto orderAux = new OrderDto();
+
+        for ( Order order : orders )
+        {
+            orderAux.setClientId( order.getUser().getUserId() );
+            orderAux.setDelivery( order.isDelivery() );
+            orderAux.setId( order.getId() );
+            orderAux.setProductList( order.getProducts() );
+            orderAux.setTaking( order.isTaking() );
+            orderAux.setUserName( order.getUser().getUsername() );
+
+            orderDto.add( orderAux );
+        }
+
+        return orderDto;
+    }
+
+    public Order findById( OrderDto orderDto )
+    {
+        return iOrderRepository.findById( orderDto.getId() ).orElseThrow();
     }
 
     public Order createOrder( OrderDto orderDto )
     {
         Order order = new Order();
+        User userOrder = userService.findById( orderDto.getClientId() );
+        List<Product> products = productService.findByIds( orderDto.getProductsIds() );
+        
+        order.setDate( generateDate() );
+        order.setDelivery( false );
+        order.setTaking( false );
 
-        order.setDelivery(false);
-        order.setProducts( order.getProducts() );
-        order.setTaking(false);
+        order.setUser( userOrder );
+        order.setProducts( products );
 
-        return iOrderRepository.save(order);
+        return iOrderRepository.save( order );
+    }
+    
+    public Order editOrder( OrderDto orderDto )
+    {
+        Order order = findById( orderDto );
+        User userOrder = userService.findById( orderDto.getClientId() );
+        List<Product> products = productService.findByIds( orderDto.getProductsIds() );
+
+        order.setId( orderDto.getId() );
+        order.setDate( generateDate() );
+        order.setDelivery( orderDto.isDelivery() );
+        order.setTaking( orderDto.isTaking() );
+
+        order.setUser( userOrder );
+        order.setProducts( products );
+
+        return iOrderRepository.save( order );    
     }
 
-    // edit: TODO
+    public Order updateOrderAdmin( OrderDto orderDto )
+    {
+        Order order = findById( orderDto );
+
+        order.setDelivery( orderDto.isDelivery() );
+        order.setTaking( orderDto.isTaking() );
+
+        return iOrderRepository.save( order );    
+    }
 
     public void deleteOrder( OrderDto orderDto )
     {
         iOrderRepository.deleteById( orderDto.getId() );
     }
 
-    
+
+    private String generateDate()
+    {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd-yy hh:mm a");
+        String date = simpleDateFormat.format( new Date() );
+        return date;
+    }
 }
--- src/main/java/com/eco/fooddelivery/service/ProductService.java
+++ src/main/java/com/eco/fooddelivery/service/ProductService.java
@@ -1,7 +1,11 @@
 package com.eco.fooddelivery.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.eco.fooddelivery.dto.ProductDto;
+import com.eco.fooddelivery.model.Product;
 import com.eco.fooddelivery.repository.IProductRepository;
 
 @Service
@@ -14,5 +18,48 @@ public class ProductService
         this.iProductRepository = iProductRepository;
     }
 
-    
+    public List<Product> findAll()
+    {
+        return iProductRepository.findAll();
+    }
+
+    private Product findById( ProductDto productDto )
+    {
+        return iProductRepository.findById( productDto.getId() ).orElseThrow();
+    }
+
+    public List<Product> findByIds( List<Long> ids )
+    {
+        return iProductRepository.findAllById(ids);
+    }
+
+    public Product createProduct( ProductDto productDto )
+    {
+        Product product = new Product();
+
+        product.setImg( productDto.getImg() );
+        product.setName( productDto.getName() );
+        product.setPrice( productDto.getPrice() );
+        product.setDescription( productDto.getDescription() );
+
+        return iProductRepository.save( product );
+    }
+
+    public Product editProduct( ProductDto productDto )
+    {
+        Product product = findById( productDto );
+
+        product.setId( productDto.getId() );
+        product.setImg( productDto.getImg() );
+        product.setName( productDto.getName() );
+        product.setPrice( productDto.getPrice() );
+        product.setDescription( productDto.getDescription() );
+
+        return iProductRepository.save( product );
+    }
+
+    public void deleteProduct( ProductDto productDto )
+    {
+        iProductRepository.deleteById( productDto.getId() );
+    }
 }
