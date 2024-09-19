8d87edea30231e36d4cf24a46533cadf4b120a0c
Nicolas <nicolas.echenique.117@gmail.com>
2024-09-19 06:36:04 -0300

    Controladores listos

--- src/main/java/com/eco/fooddelivery/controller/OrderController.java
+++ src/main/java/com/eco/fooddelivery/controller/OrderController.java
@@ -1,12 +1,92 @@
 package com.eco.fooddelivery.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eco.fooddelivery.dto.Message;
+import com.eco.fooddelivery.dto.OrderDto;
+import com.eco.fooddelivery.model.Order;
+import com.eco.fooddelivery.service.OrderService;
 
 /** Administracion de las Ordenes */
 @RestController
 @RequestMapping("/orders")
 public class OrderController
 {
-    
+    private OrderService orderService;
+
+    public OrderController( OrderService orderService )
+    {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity< List<OrderDto> > getAllOrders()
+    {
+        List<OrderDto> orders = orderService.findAll();
+        return new ResponseEntity<>( orders, HttpStatus.FOUND );
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createOrder( @RequestBody OrderDto orderDto )
+    {
+        orderService.createOrder( orderDto );
+
+        return new ResponseEntity<>( HttpStatus.OK );
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<?> editOrder( @RequestBody OrderDto orderDto )
+    {
+        Order orderStatus = orderService.findById( orderDto );
+        if ( orderStatus.isTaking() )
+        {
+            if ( orderStatus.isDelivery() )
+            {
+                return new ResponseEntity<>( new Message("Order in delivery, can't edit"), HttpStatus.NOT_ACCEPTABLE );
+            }
+
+            return new ResponseEntity<>( new Message("Order taking, can't edit"), HttpStatus.NOT_ACCEPTABLE );
+        }
+
+        orderService.editOrder(orderDto);
+
+        return new ResponseEntity<>( new Message("Order edited"), HttpStatus.OK  );
+    }
+
+    @PutMapping("/update-admin")
+    public ResponseEntity<?> updateOrderAdmin( @RequestBody OrderDto orderDto )
+    {
+        orderService.updateOrderAdmin( orderDto );
+
+        return new ResponseEntity<>( new Message("Order edited"), HttpStatus.OK  );
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteOrder( @RequestBody OrderDto orderDto )
+    {
+        Order orderStatus = orderService.findById( orderDto );
+        if ( orderStatus.isTaking() )
+        {
+            if ( orderStatus.isDelivery() )
+            {
+                return new ResponseEntity<>( new Message("Order in delivery"), HttpStatus.OK );
+            }
+
+            return new ResponseEntity<>( new Message("Order taking"), HttpStatus.OK );
+        }
+
+        orderService.deleteOrder( orderDto );
+
+        return new ResponseEntity<>( new Message("Order remove"), HttpStatus.OK  );
+    }
 }
--- src/main/java/com/eco/fooddelivery/controller/ProductController.java
+++ src/main/java/com/eco/fooddelivery/controller/ProductController.java
@@ -1,12 +1,62 @@
 package com.eco.fooddelivery.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eco.fooddelivery.dto.ProductDto;
+import com.eco.fooddelivery.model.Product;
+import com.eco.fooddelivery.service.ProductService;
 
 /** Administracion de los productos */
 @RestController
 @RequestMapping("/products")
 public class ProductController
 {
-    
+    private ProductService productService;
+
+    public ProductController( ProductService productService )
+    {
+        this.productService = productService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity< List<Product> > getAll()
+    {
+        List<Product> products = productService.findAll();
+
+        return new ResponseEntity<>( products, HttpStatus.OK );
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createProduct( @RequestBody ProductDto productDto )
+    {
+        productService.createProduct( productDto );
+
+        return new ResponseEntity<>( HttpStatus.OK );
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<?> editProduct( @RequestBody ProductDto productDto )
+    {
+        productService.editProduct( productDto );
+
+        return new ResponseEntity<>( HttpStatus.OK );
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteProduct( @RequestBody ProductDto productDto )
+    {
+        productService.deleteProduct( productDto );
+
+        return new ResponseEntity<>( HttpStatus.OK );
+    }
 }
